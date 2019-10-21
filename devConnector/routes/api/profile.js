17fdae513b7c75e6094bd7f1a24e8247f098adb8
Panagiotis Ganelis <ganelis.pan@gmail.com>
2019-10-21 16:09:54 +0200

    fix experience bug and update experience

--- devConnector/routes/api/profile.js
+++ devConnector/routes/api/profile.js
@@ -63,7 +63,7 @@ router.post('/', [auth, [
         
         if (profile) {
             // Update
-            profile = await Profile.findOneAndDelete({ user: req.user.id }, { $set: profileFIelds}, { new: true });
+            profile = await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields}, { new: true });
             return res.json(profile);
         }
         
@@ -75,7 +75,6 @@ router.post('/', [auth, [
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server Error');
-
     }
 });
 
@@ -128,6 +127,36 @@ router.delete('/', auth, async(req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+
+// @route PUT api/profile/experience
+// @desc Add profile experience
+// @access Private
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required')
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: error.array() });
+    }
+
+    const { title, company, location, from, to, current, description } = req.body;
+    const newExp = { title, company, location, from, to, current, description };
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        
+        console.log(profile);
+        
+        profile.experience.unshift(newExp);
+        await profile.save();
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 
 module.exports = router;
