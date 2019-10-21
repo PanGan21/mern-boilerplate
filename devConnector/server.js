628443356a792ac1ce2799e38b5db139aefa0afe
Panagiotis Ganelis <ganelis.pan@gmail.com>
2019-10-21 14:29:47 +0200

    Add authentication, profile, user, auth routes

--- devConnector/server.js
+++ devConnector/server.js
@@ -6,7 +6,16 @@ const app = express();
 // Connect Database
 connectDB();
 
+// initialize middleware
+app.use(express.json({ extended: false}));
+
 app.get('/', (req, res) => res.send('API running'));
+
+// define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
