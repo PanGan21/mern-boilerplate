167088feebfbb1328de8a6e35e3912ed3db3b179
Panagiotis Ganelis <ganelis.pan@gmail.com>
2019-10-28 23:55:43 +0100

    create the register component in the frontend

--- devConnector/client/src/components/auth/Register.js
+++ devConnector/client/src/components/auth/Register.js
@@ -12,16 +12,25 @@ const Register = () => {
 
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
+    const onSubmit = async e => {
+        e.preventDefault();
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            console.log('Success');
+        }
+    }
+
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-            <form className="form" action="create-profile.html">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                     <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
                 </div>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" />
+                    <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
                     <small className="form-text"
                         >This site uses Gravatar so if you want a profile image, use a
                         Gravatar email</small
@@ -33,6 +42,8 @@ const Register = () => {
                         placeholder="Password"
                         name="password"
                         minLength="6"
+                        value={password} onChange={e => onChange(e)}
+                        required
                     />
                 </div>
                 <div className="form-group">
@@ -41,6 +52,8 @@ const Register = () => {
                         placeholder="Confirm Password"
                         name="password2"
                         minLength="6"
+                        value={password2} onChange={e => onChange(e)}
+                        required
                     />
                     </div>
                 <input type="submit" className="btn btn-primary" value="Register" />
