01e5fd42bce1520a5a98ff762aab67b175e39881
Panagiotis Ganelis <ganelis.pan@gmail.com>
2019-11-05 18:16:58 +0100

    redirect to dashboard after login/register

--- devConnector/client/src/components/auth/Login.js
+++ devConnector/client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { login }from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [ formData, setFormData ] = useState({
         email: '',
         password: ''
@@ -13,7 +16,12 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log('Success');
+        login(email, password);
+    }
+
+    // Redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to='/dashboard' />
     }
 
     return (
@@ -24,6 +32,9 @@ const Login = () => {
                 <div className="form-group">
                     <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} required />
                 </div>
+                <div className="form-group">
+                    <input type="password" placeholder="password" name="password" value={password} onChange={e => onChange(e)} minLength='6' required />
+                </div>
                 <input type="submit" className="btn btn-primary" value="Login" />
             </form>
             <p className="my-1">
@@ -33,4 +44,13 @@ const Login = () => {
     )
 }
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
